fcd8d77d731955efaac40abb3efc52bc6883319a
Felix Heim <fmheim@kth.se>
2023-04-01 14:12:08 +0200

    improved colors for input field cursor and selection

--- androidApp/src/main/java/com/example/vocabs_kmm/android/vocab_to_flashcard/presentation/components/VocabInputFieldBox.kt
+++ androidApp/src/main/java/com/example/vocabs_kmm/android/vocab_to_flashcard/presentation/components/VocabInputFieldBox.kt
@@ -4,12 +4,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -34,28 +37,41 @@ fun VocabInputFieldBox(
     val focusManager = LocalFocusManager.current
 
     Box(modifier = modifier, contentAlignment = Alignment.Center){
-        OutlinedTextField(
-            singleLine = true,
-            shape = CircleShape,
-            placeholder = {Text(text = "type a word...", textAlign = TextAlign.Center)},
-            value = state.vocabInput ?: "",
-            textStyle = MaterialTheme.typography.h2,
-            onValueChange = { newValue -> onEvent(VocabToFlashcardEvent.VocabInputChanged(text = newValue)) },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Text,
-                imeAction = ImeAction.Go
-            ),
-            keyboardActions = KeyboardActions {
-                keyboardController?.hide()
-                focusManager.clearFocus()
-                onEvent(VocabToFlashcardEvent.GeneratePhrase)
-            },
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                textColor = MaterialTheme.colors.onPrimary,
-                backgroundColor = MaterialTheme.colors.primary,
-                placeholderColor = MaterialTheme.colors.onPrimary.copy(alpha = 0.7f)
-            )
+        val customTextSelectionColors = TextSelectionColors(
+            handleColor = MaterialTheme.colors.surface.copy(alpha = 0.8f),
+            backgroundColor = MaterialTheme.colors.onPrimary.copy(alpha = 0.3f),
         )
+
+        CompositionLocalProvider(
+            LocalTextSelectionColors provides customTextSelectionColors,
+        ){
+            OutlinedTextField(
+                singleLine = true,
+                shape = CircleShape,
+                placeholder = {Text(text = "type a word...", textAlign = TextAlign.Center)},
+                value = state.vocabInput ?: "",
+                textStyle = MaterialTheme.typography.h2,
+                onValueChange = { newValue -> onEvent(VocabToFlashcardEvent.VocabInputChanged(text = newValue)) },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Go
+                ),
+                keyboardActions = KeyboardActions {
+                    keyboardController?.hide()
+                    focusManager.clearFocus()
+                    onEvent(VocabToFlashcardEvent.GeneratePhrase)
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = MaterialTheme.colors.onPrimary,
+                    unfocusedBorderColor = MaterialTheme.colors.onPrimary,
+                    textColor = MaterialTheme.colors.onPrimary,
+                    backgroundColor = MaterialTheme.colors.primary,
+                    placeholderColor = MaterialTheme.colors.onPrimary.copy(alpha = 0.7f),
+                    cursorColor = MaterialTheme.colors.onPrimary.copy(alpha =0.5f)
+                )
+            )
+        }
+
     }
 }
 
