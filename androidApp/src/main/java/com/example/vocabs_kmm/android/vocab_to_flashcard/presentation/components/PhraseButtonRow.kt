47211c604733ea0f5d394614dcc9c07f80ddf471
Felix Heim <fmheim@kth.se>
2023-04-01 14:10:13 +0200

    added coil and refactored dropdown

--- androidApp/src/main/java/com/example/vocabs_kmm/android/vocab_to_flashcard/presentation/components/PhraseButtonRow.kt
+++ androidApp/src/main/java/com/example/vocabs_kmm/android/vocab_to_flashcard/presentation/components/PhraseButtonRow.kt
@@ -2,7 +2,6 @@ package com.example.vocabs_kmm.android.vocab_to_flashcard.presentation.component
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -11,18 +10,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Autorenew
 import androidx.compose.material.icons.filled.Check
@@ -30,17 +24,16 @@ import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.vocabs_kmm.android.R
 import com.example.vocabs_kmm.android.core.presentation.theme.DarkPurple
 import com.example.vocabs_kmm.android.core.presentation.theme.LightGreen
 import com.example.vocabs_kmm.android.core.presentation.theme.VocabsTheme
-import com.example.vocabs_kmm.core.presentation.UiLanguage
+import com.example.vocabs_kmm.android.core.presentation.widgets.LanguageDropDown
 import com.example.vocabs_kmm.vocab_to_flashcard.presentation.VocabToFlashcardEvent
 import com.example.vocabs_kmm.vocab_to_flashcard.presentation.VocabToFlashcardState
 
@@ -61,32 +54,10 @@ fun BoxScope.PhraseButtonsRow(
         SaveIconButton(onEvent, state)
 
     }
-    DropdownMenu(modifier = Modifier.background(
-        Brush.horizontalGradient(
-            colors = listOf(
-                MaterialTheme.colors.surface, MaterialTheme.colors.background
-            )
-        )
-    ),
-        expanded = state.isChoosingLanguage,
-        onDismissRequest = { onEvent(VocabToFlashcardEvent.CloseLanguageDropDown) }) {
-        UiLanguage.allLanguages.forEach {
-            DropdownMenuItem(onClick = {
-                onEvent(VocabToFlashcardEvent.SelectLanguage(it))
-                onEvent(VocabToFlashcardEvent.CloseLanguageDropDown)
-            }) {
-                Image(
-                    painterResource(id = it.drawableRes),
-                    contentDescription = it.language.langName,
-                    modifier = Modifier.size(30.dp),
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(text = it.language.langName)
-            }
-
-
-        }
-    }
+    LanguageDropDown(
+        isExpanded = state.isChoosingLanguage,
+        onLanguageSelected = { language -> onEvent(VocabToFlashcardEvent.SelectLanguage(language) )},
+        close = {onEvent(VocabToFlashcardEvent.CloseLanguageDropDown)})
 }
 
 @OptIn(ExperimentalAnimationApi::class)
@@ -147,8 +118,8 @@ private fun RowScope.LanguageIconButton(
             .border(width = 3.dp, color = MaterialTheme.colors.primary, shape = CircleShape)
 
     ) {
-        Image(
-            painter = painterResource(id = state.selectedLanguage.drawableRes),
+        AsyncImage(
+            model = state.selectedLanguage.drawableRes,
             contentDescription = "selectedLanguage",
             modifier = Modifier.size(60.dp),
         )
