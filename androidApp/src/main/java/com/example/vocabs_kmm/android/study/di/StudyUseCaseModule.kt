0e2803dbd4aebc5cc11d9c9b6546841044e76542
Felix Heim <fmheim@kth.se>
2023-04-01 14:11:29 +0200

    small refactor

--- androidApp/src/main/java/com/example/vocabs_kmm/android/study/di/StudyUseCaseModule.kt
+++ androidApp/src/main/java/com/example/vocabs_kmm/android/study/di/StudyUseCaseModule.kt
@@ -1,13 +1,12 @@
 package com.example.vocabs_kmm.android.study.di
 
-import com.example.study.domain.flashcard.GetRandomFlashcard
+import com.example.vocabs_kmm.study.domain.flashcard.GetRandomFlashcard
 import com.example.vocabs_kmm.core.domain.flashcard.FlashcardDataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -15,7 +14,7 @@ object StudyUseCaseModule {
 
     @Provides
     @ViewModelScoped
-    fun provideGetRandomFlashCardUseCase(dataSource: FlashcardDataSource): GetRandomFlashcard{
+    fun provideGetRandomFlashCardUseCase(dataSource: FlashcardDataSource): GetRandomFlashcard {
         return GetRandomFlashcard(flashcardDataSource = dataSource)
     }
 
